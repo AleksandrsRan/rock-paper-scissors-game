79e962bcc498ec81b341503d735c823ecebd4c8b
AleksandrsRan <114050733+AleksandrsRan@users.noreply.github.com>
2022-09-21 09:06:24 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,15 @@
+human_turn = input('Input (rock, paper or scissors):')
+computer_turn = input('sbf')
+
+if human_turn == 'rock' 'paper' 'scissors':
+    print('neder')
+elif human_turn == computer_turn:
+    print('Draw!')
+elif human_turn == 'rock' and computer_turn == 'scissors':
+    print('You win!')
+elif human_turn == 'paper' and computer_turn == 'rock':
+    print('You wins')
+elif human_turn == 'scissors' and computer_turn == 'paper':
+    print('You win!')
+else:
+    print('You lose!')
