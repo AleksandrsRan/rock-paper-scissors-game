639e6a3567a3b5edef1c81a4b71536e4eb3e8bed
AleksandrsRan <114050733+AleksandrsRan@users.noreply.github.com>
2022-09-21 10:09:35 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,15 +1,32 @@
-human_turn = input('Input (rock, paper or scissors):')
-computer_turn = input('sbf')
+def program():
+    print('ROCK PAPER SCISSORS')
+    human_turn = input('Input your turn:')
 
-if human_turn == 'rock' 'paper' 'scissors':
-    print('neder')
-elif human_turn == computer_turn:
-    print('Draw!')
-elif human_turn == 'rock' and computer_turn == 'scissors':
-    print('You win!')
-elif human_turn == 'paper' and computer_turn == 'rock':
-    print('You wins')
-elif human_turn == 'scissors' and computer_turn == 'paper':
-    print('You win!')
-else:
-    print('You lose!')
+    import random
+    turns = ['rock', 'paper', 'scissors']
+    computer_turn = random.choice(turns)
+    print('Computer:', computer_turn)
+
+
+    if human_turn == computer_turn:
+        print('Draw!')
+    elif human_turn == 'rock' and computer_turn == 'scissors':
+        print('You win!')
+    elif human_turn == 'paper' and computer_turn == 'rock':
+        print('You wins')
+    elif human_turn == 'scissors' and computer_turn == 'paper':
+        print('You win!')
+    elif human_turn == 'scissors' and computer_turn == 'rock':
+        print('You lose!')
+    elif human_turn == 'rock' and computer_turn == 'paper':
+        print('You lose')
+    elif human_turn == 'paper' and computer_turn == 'scissors':
+        print('You lose!')
+    else:
+        print('Next time input rock, paper or scissors')
+flag = True
+while flag:
+    program()
+    flag = input('Would you like to run the program again? [y/n]') == 'y'
+
+print ("The program will now stop.")
