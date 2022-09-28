e9bc363de99a49bcc79b495ea734e51b2820d066
AleksandrsRan <114050733+AleksandrsRan@users.noreply.github.com>
2022-09-28 09:36:06 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,12 +1,20 @@
+from random import random
+
+import random
+turns = ['rock', 'paper', 'scissors']
+human_turns = []
+computer_turns = []
+
+
 def program():
     print('ROCK PAPER SCISSORS')
+    
     human_turn = input('Input your turn:')
-
-    import random
-    turns = ['rock', 'paper', 'scissors']
     computer_turn = random.choice(turns)
     print('Computer:', computer_turn)
 
+    human_turns .append (human_turn)
+    computer_turns .append (computer_turn)
 
     if human_turn == computer_turn:
         print('Draw!')
@@ -16,17 +24,15 @@ def program():
         print('You wins')
     elif human_turn == 'scissors' and computer_turn == 'paper':
         print('You win!')
-    elif human_turn == 'scissors' and computer_turn == 'rock':
-        print('You lose!')
-    elif human_turn == 'rock' and computer_turn == 'paper':
-        print('You lose')
-    elif human_turn == 'paper' and computer_turn == 'scissors':
-        print('You lose!')
     else:
-        print('Next time input rock, paper or scissors')
+        print('You lose')
 flag = True
 while flag:
     program()
-    flag = input('Would you like to run the program again? [y/n]') == 'y'
+    flag = input('Would you like to play again? [y/n]') == 'y'
 
-print ("The program will now stop.")
+
+print ('You played', len(human_turns), 'times')
+print (human_turns)
+print (computer_turns)
+print ('The game will now end.')
